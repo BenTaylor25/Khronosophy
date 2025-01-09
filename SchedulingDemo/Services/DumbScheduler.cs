467b482c491aa8f4fb9a262590e55673d6df9071
benlaptop25 <ben.taylor2573@gmail.com>
2025-01-09 16:07:48 +0000

    created file for DumbScheduler Service

---
+++ SchedulingDemo/Services/DumbScheduler.cs
@@ -0,0 +1,13 @@
+
+using SchedulingDemo.Models;
+
+namespace SchedulingDemo.Services;
+
+public class DumbScheduler : IScheduler
+{
+    public void ScheduleUsersTasks(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
+
