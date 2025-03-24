864868d4ad3485d7fbef25fc1d562db24bdd8e9d
benlaptop25 <ben.taylor2573@gmail.com>
2025-03-24 18:05:50 +0000

    ETF Service Validation

---
+++ Calendar/Constants/ETFConstants.cs
@@ -0,0 +1,17 @@
+
+using Calendar.Models.Events;
+
+namespace Calendar.Constants;
+
+public static class ETFConstants
+{
+    // 24 hours in minutes...
+    public const int MAX_MINIMUM_EVENT_DURATION_MINUTES = 1440;
+
+    public const int MIN_TASK_IMPORTANCE = 1;
+    public const int MAX_TASK_IMPORTANCE = 10;
+
+    public const int MIN_TASK_INTENSITY = 1;
+    public const int MAX_TASK_INTENSITY = 10;
+}
+
--- Calendar/Services/SchedulingService/ETF/ETFService.cs
+++ Calendar/Services/SchedulingService/ETF/ETFService.cs
@@ -1,6 +1,7 @@
 using ErrorOr;
 
 using Calendar.Models;
+using Calendar.Constants;
 
 namespace Calendar.Services.SchedulingService.ETF;
 
@@ -8,6 +9,70 @@ public class ETFService : IETFService
 {
     public ErrorOr<Success> ScheduleEvents(KhronosophyUser user)
     {
+        ErrorOr<Success> validation = ParameterValidation(user);
+
+        if (validation.IsError)
+        {
+            return validation.Errors;
+        }
+
+        // tasks by importance
+
+        // time in working day
+
+        // schedule for day
+
+        return new Success();
+    }
+
+    private ErrorOr<Success> ParameterValidation(KhronosophyUser user)
+    {
+        if (
+            user.DayStart == null ||
+            user.DayEnd == null ||
+            user.MinimumEventDurationMinutes == null
+        )
+        {
+            return Error.Validation("User settings unset");
+        }
+
+        if (
+            // day start divisible by 15 mins
+            // day end divisible by 15 mins
+            user.DayStart >= user.DayEnd ||
+            user.MinimumEventDurationMinutes <= 0 ||
+            user.MinimumEventDurationMinutes >
+                ETFConstants.MAX_MINIMUM_EVENT_DURATION_MINUTES ||
+            user.MinimumEventDurationMinutes % 15 != 0
+        )
+        {
+            return Error.Validation("User settings invalid");
+        }
+
+        foreach (TaskboardTask taskboardTask in user.Taskboard.Tasks)
+        {
+            if (
+                taskboardTask.Importance == null ||
+                taskboardTask.Intensity == null
+            )
+            {
+                return Error.Validation("Task properties unset");
+            }
+
+            if (
+                taskboardTask.Importance <
+                    ETFConstants.MIN_TASK_IMPORTANCE ||
+                taskboardTask.Importance >
+                    ETFConstants.MAX_TASK_IMPORTANCE ||
+                taskboardTask.Intensity <
+                    ETFConstants.MIN_TASK_INTENSITY ||
+                taskboardTask.Intensity >
+                    ETFConstants.MAX_TASK_INTENSITY
+            )
+            {
+                return Error.Validation("Task properties invalid");
+            }
+        }
 
         return new Success();
     }
