49fde538d1d2b88cda5bbd68973cd64581969460
benlaptop25 <ben.taylor2573@gmail.com>
2025-03-27 16:16:44 +0000

    ETF Scheduling Service

--- Calendar/Services/SchedulingService/ETF/ETFService.cs
+++ Calendar/Services/SchedulingService/ETF/ETFService.cs
@@ -1,6 +1,7 @@
 using ErrorOr;
 
 using Calendar.Models;
+using Calendar.Models.Events;
 using Calendar.Constants;
 
 namespace Calendar.Services.SchedulingService.ETF;
@@ -30,7 +31,12 @@ public class ETFService : IETFService
 
         while (CalendarHelpers.ShouldScheduleTasks(tasksByImportance))
         {
-            ScheduleEventsForDay(user, tasksByImportance, dateToSchedule);
+            ScheduleEventsForDay(
+                user,
+                tasksByImportance,
+                workingDayEnd,
+                dateToSchedule
+            );
 
             dateToSchedule = dateToSchedule.AddDays(1);
         }
@@ -41,10 +47,80 @@ public class ETFService : IETFService
     private void ScheduleEventsForDay(
         KhronosophyUser user,
         List<TaskboardTask> tasksByImportance,
+        TimeOnly workingDayEnd,
         DateOnly date
     )
     {
+        TimeOnly? nextEventStart = GetNextEventStart(
+            date,
+            user.DayStart!.Value,
+            user.DayEnd!.Value,
+            TimeSpan.FromMinutes(user.MinimumEventDurationMinutes!.Value)
+        );
 
+        if (nextEventStart == null)
+        {
+            return;
+        }
+
+        TimeSpan timeUntilEndOfWorkingDay =
+            workingDayEnd - nextEventStart!.Value;
+
+        foreach (TaskboardTask taskboardTask in tasksByImportance)
+        {
+            if (
+                timeUntilEndOfWorkingDay.TotalMinutes >=
+                user.MinimumEventDurationMinutes
+            )
+            {
+                TimeSpan taskRemainingDuration =
+                    RemainingDuration(taskboardTask);
+
+                // Min(taskRemainingDuration, timeUntilEndOfWorkingDay);
+                TimeSpan eventDuration =
+                    taskRemainingDuration > timeUntilEndOfWorkingDay ?
+                    timeUntilEndOfWorkingDay : taskRemainingDuration;
+
+                DateTime startDateTime = new(date, nextEventStart!.Value);
+
+                ScheduledEvent scheduledEvent = new(
+                    taskboardTask.Name,
+                    startDateTime,
+                    startDateTime + eventDuration,
+                    taskboardTask.Id
+                );
+
+                taskboardTask.Events.Add(scheduledEvent);
+                user.EventCalendar.Events.Add(scheduledEvent);
+
+                nextEventStart =
+                    TimeOnly.FromDateTime(scheduledEvent.EndDateTime)
+                        .Add(
+                            ETFConstants.BREAK_DURATION,
+                            out int _wrappedDays
+                        );
+
+                timeUntilEndOfWorkingDay -=
+                    eventDuration + ETFConstants.BREAK_DURATION;
+            }
+        }
+    }
+
+    private TimeSpan RemainingDuration(TaskboardTask task)
+    {
+        TimeSpan remainingDuration = task.ExpectedDuration;
+
+        foreach (ScheduledEvent scheduledEvent in task.Events)
+        {
+            remainingDuration -= scheduledEvent.Duration;
+        }
+
+        if (remainingDuration < TimeSpan.Zero)
+        {
+            remainingDuration = TimeSpan.Zero;
+        }
+
+        return remainingDuration;
     }
 
     private TimeOnly CalculateWorkingDayEnd(
@@ -130,5 +206,60 @@ public class ETFService : IETFService
         }
 
         return new Success();
+    }
+
+    private TimeOnly? GetNextEventStart(
+        DateOnly date,
+        TimeOnly dayStart,
+        TimeOnly dayEnd,
+        TimeSpan minimumEventDuration
+    )
+    {
+        // We want to restrict this function to a specified date.
+        // This variable keeps track of if we've crossed into
+        // the next date.
+        int _wrappedDays;
+
+        TimeOnly nextEventStart = dayStart;
+
+        DateTime now = DateTime.UtcNow;
+        DateOnly dateNow = DateOnly.FromDateTime(now);
+        TimeOnly timeNow = TimeOnly.FromDateTime(now);
+
+        // If we're scheduling for today and we're already
+        // past dayStart, we'll be scheduling for the past.
+        // Correct this by shifting the nextEventStart to
+        // the next whole hour.
+        if (date == dateNow && timeNow > nextEventStart)
+        {
+            nextEventStart = new TimeOnly(
+                timeNow.Hour,
+                0,
+                0
+            )
+                .AddHours(1, out _wrappedDays);
+
+            #region Error Handling
+            if (_wrappedDays != 0)
+            {
+                return null;
+            }
+            #endregion
+        }
+
+        // If there is not enough time in the day to satisfy the
+        // minimum event time, return null.
+        if (
+            nextEventStart.Add(
+                minimumEventDuration,
+                out _wrappedDays
+            ) > dayEnd ||
+            _wrappedDays != 0
+        )
+        {
+            return null;
+        }
+
+        return nextEventStart;
     }
 }
